b62b79fa835427598ff06f094d7d9b7298c33cc9
Jared Koester <jaredjkoester@gmail.com>
2017-11-11 17:27:42 -0500

    added simple movement script to the Main Camera component

---
+++ OcuSurg/Assets/MovementController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 4.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 4.0f;
+
+		transform.Translate(x, 0, 0);
+		transform.Translate(0, 0, z);
+	}
+}
